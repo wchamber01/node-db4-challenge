1d2a4185c6de085fd2cc97bb82152df4df4c73b2
wchamber01 <williamchambers@gmail.com>
2020-01-23 18:14:23 -0500

    added recipe_ingredients to seeds file

--- data/seeds/seeds.js
+++ data/seeds/seeds.js
@@ -57,24 +57,46 @@ exports.seed = function(knex) {
         },
         {
           id: 10,
-          name: "rosemary"
-        },
-        {
-          id: 11,
-          name: "bacon"
-        },
-        {
-          id: 12,
-          name: "brown sugar"
-        },
-        {
-          id: 13,
           name: "salt"
         },
         {
-          id: 14,
+          id: 11,
           name: "pepper"
+        },
+        {
+          id: 12,
+          name: "rosemary"
+        },
+        {
+          id: 13,
+          name: "bacon"
+        },
+        {
+          id: 14,
+          name: "brown sugar"
         }
+      ]);
+    })
+    .then(() => {
+      return knex("recipe_ingredients").insert([
+        { recipe_id: 1, ingredient_id: 1, qty: "1/4", units: "cup/s" },
+        { recipe_id: 1, ingredient_id: 2, qty: "1/4", units: "cup/s" },
+        { recipe_id: 1, ingredient_id: 3, qty: "1/4", units: "cup/s" },
+        { recipe_id: 1, ingredient_id: 4, qty: "1", units: "tbls" },
+        { recipe_id: 1, ingredient_id: 5, qty: "4", units: "cup/s" },
+        { recipe_id: 1, ingredient_id: 10, qty: "1/4", units: "tsp" },
+        { recipe_id: 1, ingredient_id: 11, qty: "1/4", units: "tsp" },
+        { recipe_id: 2, ingredient_id: 5, qty: "8", units: "each" },
+        { recipe_id: 2, ingredient_id: 6, qty: "1/4", units: "cup/s" },
+        { recipe_id: 2, ingredient_id: 7, qty: "1/2", units: "cup/s" },
+        { recipe_id: 2, ingredient_id: 8, qty: "1", units: "tsp" },
+        { recipe_id: 2, ingredient_id: 9, qty: "1", units: "tbls" },
+        { recipe_id: 2, ingredient_id: 10, qty: "1/4", units: "tsp" },
+        { recipe_id: 2, ingredient_id: 11, qty: "1/4", units: "tsp" },
+        { recipe_id: 3, ingredient_id: 12, qty: "1", units: "tbls" },
+        { recipe_id: 3, ingredient_id: 13, qty: "1", units: "lb/s" },
+        { recipe_id: 3, ingredient_id: 14, qty: "1/2", units: "cup/s" },
+        { recipe_id: 3, ingredient_id: 11, qty: "1", units: "tsp" }
       ]);
     });
 };
